7d080c2a91088c7502ae4cc2a9e1331d0310991e
abiodun603 <abioduncsc@gmail.com>
2022-02-15 16:24:49 +0100

    logo

--- src/screens/reset/Reset.js
+++ src/screens/reset/Reset.js
@@ -41,7 +41,7 @@ const Reset = () => {
         <>
             <LoginWrapper>
             <LoginBanner>
-                    <BannerLogo src = {authLogo} alt="" />
+            <BannerLogo src = {authLogo} alt=""  style = {{zIndex: 999 , width: 200}}/>
                     <div style={{display: 'flex', alignItems: "center",flexDirection: 'column',justifyContent: "center", width: "100%", height: "100%"}}>
                         <span>Welcome to</span>
                         <h1>FIXONTIME</h1>
